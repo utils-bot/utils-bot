54777a74d8361b4d8f39fb44db4c3b52b4d89bc2
khoi1908vn <khoimle7@gmail.com>
2023-02-26 09:35:24 +0700

    update version 0.1.2

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ class configurations:
     beta = True
     max_global_ratelimit = 5
     default_maintenance_status = False
-    code_version = 'v0.1.1'
+    code_version = 'v0.1.2'
 
 intents = Intents.default()
 intents.members = True
