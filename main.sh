044a7fcdabb30b9e0218b607158cd78e5e30f976
khoi1908vn <khoimle7@gmail.com>
2023-03-09 00:44:48 +0700

    revert some changes

--- main.sh
+++ main.sh
@@ -2,6 +2,5 @@ echo ========================================== Updating git ===================
 git pull
 git reset --hard origin/main
 sleep 5
-clear
 echo ========================================== Starting ==========================================
 python3 main.py
