e408be3289f532834f3248c07c89e62e742c6aaa
khoi1908vn <khoimle7@gmail.com>
2023-04-02 09:25:45 +0700

    fix shit

--- main.sh
+++ main.sh
@@ -1,12 +1,16 @@
-python -m pip install -r requirements.txt
 if [ -n "$NO_GIT_AUTOMATION" ] && [ "$NO_GIT_AUTOMATION" = "YES" ]; then
   echo ========================================== Updating git ==========================================
   git pull
   git reset --hard origin/main
   sleep 5
-  echo ========================================== Starting ==========================================
-  python3 main.py
-else
-  echo ========================================== Starting ==========================================
-  python3 main.py
 fi
+
+if [ -n "$NO_PACKAGE_INSTALLED" ] && [ "$NO_PACKAGE_INSTALLED" = "YES" ]; then
+  apt-get update && apt-get install -y python3-pip
+fi
+
+echo ========================================== Starting ==========================================
+python -m pip install -r requirements.txt
+python3 main.py
+
+
