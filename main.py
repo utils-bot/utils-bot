90a4b4f96e6f98f23f33e0e2c65659b886fd4d9e
khoi1908vn <khoimle7@gmail.com>
2023-04-25 20:50:49 +0700

    removed debug

--- main.py
+++ main.py
@@ -327,7 +327,6 @@ class game_wordle_handler():
         return {"word": word, "success": success}
     async def maingame(self, interaction: Interaction, tries: int = 6, secret_word: str = None, tried: list = []):
         if secret_word == None: secret_word = await self.get_word().get("word", "smhhh")
-        print(secret_word) # debugging
         embed = Embed(title="Wordle")
         embed.description = "Make a guess by click the green guess button below!\n`Your guesses:` ```\n" + "\n".join(tried) + "```"
         embed.set_footer(text = f'Requested by {interaction.user.name}#{interaction.user.discriminator}', icon_url=interaction.user.avatar)
