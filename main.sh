32038037bcd70214c0ca3f4a4412574fcb364794
Minh Khoi Le <khoimle7@gmail.com>
2023-07-29 11:26:47 +0700

    v0.6.2 development (#18)
    [-] build_mode() removal and replit-based stuff
    [+] Update README.md

--- main.sh
+++ main.sh
@@ -1,13 +1,2 @@
-if [ "$NO_GIT_AUTOMATION" = "YES" ]; then
-    echo ========================================== Updating git ==========================================
-    git pull
-    git reset --hard origin/main
-fi
-
-if [ "$NO_PIP_INSTALLED" = "YES" ]; then
-    echo ========================================== Installing dependencies ==========================================
-    apt-get update && apt-get install -y python3-pip
-fi
-
 echo ========================================== Starting ==========================================
 python main.py
